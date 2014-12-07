695aefd941e9414202545475f52a2d28a8dbc371
tayipturk@gmail.com <dota4evaorneva@gmail.com>
2014-12-07 21:25:15 +0200

    connected playlist to db
    first stage

--- src/com/softwareG06/sanaldersanem/OynatmaListesiAl.java
+++ src/com/softwareG06/sanaldersanem/OynatmaListesiAl.java
@@ -1,14 +1,33 @@
 package com.softwareG06.sanaldersanem;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.Statement;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
 
 public class OynatmaListesiAl extends Activity{
 
-	String kurs,ders,playlist;
+	String kurs,ders,playlist,add;
 	TextView tv;
 	 Bundle veriAl  = new  Bundle();
+	 
+	 private Connection conn = null;  
+		private String url = "jdbc:mysql://85.159.67.247:3306/";
+		private String dbName = "softwareG06";
+		private String driver = "com.mysql.jdbc.Driver";  
+		private String userName = "softwareG06"; 
+		private String password = "softwareG06";
+		
+		Statement stmt;
+		ResultSet rsSpiner;
+		ResultSetMetaData rsmdSpiner ;
+		PreparedStatement ps;
 	 
 	@Override
 	  public void onCreate(Bundle savedInstanceState) {
@@ -23,7 +42,24 @@ public class OynatmaListesiAl extends Activity{
 		
 		tv.setText("ders : "+ders+" kurs : "+kurs+" playlist : "+playlist);
 		
-		
+		try {
+			Class.forName(driver).newInstance();
+	        conn = DriverManager.getConnection(url+dbName,userName,password);
+	        stmt = conn.createStatement();
+	        add= "INSERT INTO playlist_al (kurs, ders, playlist) VALUES (?,?,?)"; 
+	        ps=(PreparedStatement) conn.prepareStatement(add);
+	        ps.setString(1,kurs);
+	        ps.setString(2,ders);
+	        ps.setString(3,playlist);
+	        ps.execute();
+	       
+	       
+	        }
+	        catch(Exception e){
+	        	
+	        }
 		
 	}
+	
+	
 }
